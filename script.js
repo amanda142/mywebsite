3a537fc60c5f84759ee431f386f1bcbd90105f78
amanda142 <arussell2@gwmail.gwu.edu>
2017-05-15 22:56:12 -0400

    More Java

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 
 	$('.grid').click(function(event){
-
 		event.preventDefault();
-
 	});
 
+
+	$('.tab').hover(function(){
+		$('.tab').removeClass('hover');
+		$(this).addClass('hover');
+	})
+
+	$('.tab').mouseleave(function(){
+		$(this).removeClass('hover');
+	})
+
+
+	$('aside img').hover(function(){
+		$(this).addClass('bigger')
+	})
+	$('aside img').mouseleave(function(){
+		$(this).removeClass('bigger')
+	})
 });
