d9f63cdbc972e82eb6500b6740b33a1b81aea24e
amanda142 <arussell2@gwmail.gwu.edu>
2017-05-17 20:21:01 -0400

    Highlight Photos, Add Footer

--- script.js
+++ script.js
@@ -5,20 +5,31 @@ $(document).ready(function() {
 	});
 
 
+	//Highlight nav tabs
 	$('.tab').hover(function(){
 		$('.tab').removeClass('hover');
 		$(this).addClass('hover');
 	})
-
 	$('.tab').mouseleave(function(){
 		$(this).removeClass('hover');
 	})
 
-
+	//Highlight home button
 	$('aside img').hover(function(){
-		$(this).addClass('bigger')
+		$(this).addClass('bigger');
 	})
 	$('aside img').mouseleave(function(){
-		$(this).removeClass('bigger')
+		$(this).removeClass('bigger');
 	})
+
+
+	//Outline photo in grid
+	$('.grid a img').hover(function(){
+		$('.grid a img').removeClass('outline');
+		$(this).addClass('outline');
+	})
+	$('.grid a img').mouseleave(function(){
+		$(this).removeClass('outline');
+	})
+
 });
